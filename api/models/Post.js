49d6656247eee72f1d35adab1576b284804d32ed
Will Ricketts <will@willricketts.com>
2015-12-21 02:46:56 +0000

    im sailing away

---
+++ api/models/Post.js
@@ -0,0 +1,30 @@
+/**
+* Post.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    title: {
+      type: 'text',
+      unique: true
+    },
+    
+    content: {
+      type: 'text'
+    },
+    
+    slug: {
+      type: 'string',
+      unique: true
+    },
+    
+    author: {
+      model: 'user'
+    }
+  }
+};
+
