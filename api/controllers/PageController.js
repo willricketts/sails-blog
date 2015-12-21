e3c5dcf6dbfb63823b3f7304baecfab86ebf74c5
Will Ricketts <will@willricketts.com>
2015-12-21 02:54:57 +0000

    adding posts to index

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -6,6 +6,20 @@
  */
 
 module.exports = {
-	
+    index: index
 };
 
+function index(req, res) {
+    Post.find({}, function(err, posts) {
+        if(err) {
+            res.send(err);
+        }
+        else {
+            var postsPayload = [];
+            for(var i in posts) {
+                postsPayload.push({ title: posts[i].title, content: posts[i].content, slug: 'post/' + posts[i].slug });
+            }
+            res.view({ posts: postsPayload });
+        }
+    });
+}
